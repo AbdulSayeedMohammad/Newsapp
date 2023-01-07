a96acd7188574fe2955d7d4a9ab51c5afdb497f8
Abdul Sayeed <117283358+AbdulSayeedMohammad@users.noreply.github.com>
2023-01-08 00:16:26 +0530

    Updates

---
+++ precache-manifest.7d2d327f692f2df9e24cee3be800ff0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de3e16c5497e9e668b04",
+    "url": "/Newsapp/static/css/main.2eb8fdd1.chunk.css"
+  },
+  {
+    "revision": "de3e16c5497e9e668b04",
+    "url": "/Newsapp/static/js/main.de3e16c5.chunk.js"
+  },
+  {
+    "revision": "2e76ef4f4b07ab805959",
+    "url": "/Newsapp/static/js/1.2e76ef4f.chunk.js"
+  },
+  {
+    "revision": "70561b3dba14c5fb87a8",
+    "url": "/Newsapp/static/js/2.70561b3d.chunk.js"
+  },
+  {
+    "revision": "b23e78fc4ea29a652b98",
+    "url": "/Newsapp/static/js/runtime~main.b23e78fc.js"
+  },
+  {
+    "revision": "bb533f76423cab3aa8f798501357e763",
+    "url": "/Newsapp/static/media/loading.bb533f76.gif"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/Newsapp/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "5ddacdb07dcd1c1cce409c6ba0b3bdb5",
+    "url": "/Newsapp/index.html"
+  }
+];
